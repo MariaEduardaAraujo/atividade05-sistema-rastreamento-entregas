eb9aaeb8e25cca4b5148e56b8eaefbb908c004dc
MariaEduardaAraujo <mariaeduardadearaujosilva@outlook.com>
2026-03-23 18:40:42 -0300

    refactor: refatoração das rotas

--- src/routes/entregas.routes.js
+++ src/routes/entregas.routes.js
@@ -1,9 +1,11 @@
 import { Router } from "express"
+import { Database } from "../database/database.js"
 import { EntregasRepository } from "../repositories/entregas.repositories.js"
 import { EntregasService } from "../services/entregas.service.js"
-import { EntregasController } from "../controller/entregas.controller.js"
+import { EntregasController } from "../controllers/entregas.controller.js"
 
-const repository = new EntregasRepository()
+const database = new Database()
+const repository = new EntregasRepository(database)
 const service = new EntregasService(repository)
 const controller = new EntregasController(service)
 
@@ -12,5 +14,8 @@ const router = Router()
 router.get("/", controller.listarTodos)
 router.get("/:id", controller.buscarPorId)
 router.post("/", controller.criar)
+router.patch("/:id/avancar", controller.avancar)
+router.patch("/:id/cancelar", controller.cancelar)
+router.get("/:id/historico", controller.historico)      
 
 export default router
