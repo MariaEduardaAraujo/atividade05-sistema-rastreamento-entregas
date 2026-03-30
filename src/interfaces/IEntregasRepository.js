75ed6e6885f2188b83c7a62c9226a5954c6ac20a
MariaEduardaAraujo <mariaeduardadearaujosilva@outlook.com>
2026-03-30 13:43:05 -0300

    feat: criação da interface IEntregasRepository

---
+++ src/interfaces/IEntregasRepository.js
@@ -0,0 +1,14 @@
+export class IEntregasRepository{
+    async listarTodos() {
+        throw new Error("Método listarTodos() não implementado")
+    }
+    async buscarPorId(id) {
+        throw new Error("Método buscarPorId() não implementado")
+    }
+    async criar(dados) {
+        throw new Error("Método criar() não implementado")
+    }
+    async atualizar(id, dados) {
+        throw new Error("Método atualizar() não implementado")
+    }
+}
