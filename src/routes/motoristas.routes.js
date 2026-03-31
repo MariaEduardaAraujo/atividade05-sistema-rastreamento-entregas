84ad805a3fa251a252b6c36fcbb2ffe0d399759b
Maria Eduarda de Araújo Silva <56840835+MariaEduardaAraujo@users.noreply.github.com>
2026-03-30 21:37:34 -0300

    Refactor: melhorando a classe motoristas.routes

--- src/routes/motoristas.routes.js
+++ src/routes/motoristas.routes.js
@@ -1,20 +1,12 @@
 import { Router } from "express"
-import { Database } from "../database/database.js"
-import { MotoristasRepository } from "../repositories/motoristas.repository.js"
-import { MotoristasService } from "../services/motoristas.service.js"
-import { MotoristasController } from "../controllers/motoristas.controller.js"
-
-const database = new Database()
-const motoristasRepository = new MotoristasRepository(database)
-const motoristasService = new MotoristasService(motoristasRepository)
-const motoristasController = new MotoristasController(motoristasService)
+import { motoristasController } from "../bootstrap.js"
 
 const router = Router()
 
 router.get("/", motoristasController.listarTodos)
 router.get("/:id", motoristasController.buscarPorId)
 router.post("/", motoristasController.criar)
-router.get("/:id/entregas")
-router.patch("/:id/atribuir")
+//router.get("/:id/entregas")
+//router.patch("/:id/atribuir")
 
 export default router
