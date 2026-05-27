cccfade96c6d79272f0d3b5dfa7efed811503170
MariaEduardaAraujo <mariaeduardadearaujosilva@outlook.com>
2026-05-27 16:37:15 -0300

    feat: adicionando novos imports ao bootstrap

--- src/bootstrap/bootstrap.js
+++ src/bootstrap/bootstrap.js
@@ -10,18 +10,24 @@ import { RelatoriosService } from "../services/relatorios.service.js"
 import { RelatoriosController } from "../controllers/relatorios.controller.js"
 import { EntregasController as PainelEntregasController } from "../controllers/painel/entregas.controller.js"
 import { MotoristasController as PainelMotoristasController } from "../controllers/painel/motoristas.controller.js"
+import { UsuariosRepository, UsuariosRepositoy} from "../repositories/usuarios.repository.prisma.js"
+import { AutenticacaoService} from "../services/auth.service.js"
+import { AutenticacaoController } from "../controllers/auth.controller.js"
 
 const entregasRepository = new EntregasRepository(database)
 const motoristasRepository = new MotoristasRepository(database)
 const relatoriosRepository = new RelatoriosRepository(database)
+const usuariosRepositoy = new UsuariosRepository(database)
 const entregasService = new EntregasService(entregasRepository, motoristasRepository)
 const motoristasService = new MotoristasService(motoristasRepository, entregasRepository)
 const relatoriosService = new RelatoriosService(relatoriosRepository)
+const autenticacaoService = new autenticacaoService(usuariosRepositoy)
 const entregasController = new EntregasController(entregasService)
 const motoristasController = new MotoristasController(motoristasService)
 const relatoriosController = new RelatoriosController(relatoriosService)
+const autenticacaoController = new AutenticacaoController(autenticacaoService)
 
 const painelEntregasController = new PainelEntregasController(entregasService, motoristasService)
 const painelMotoristasController = new PainelMotoristasController(motoristasService)
 
-export { entregasController, motoristasController, relatoriosController, painelEntregasController, painelMotoristasController }
+export { entregasController, motoristasController, relatoriosController, painelEntregasController, painelMotoristasController, autenticacaoController }
